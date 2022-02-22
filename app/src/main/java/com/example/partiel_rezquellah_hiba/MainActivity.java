6066b7217b8a928ae64ddd41d6633e763e2af38c
Hiba REZQUELLAH <hiba.rezquellah.elv@eilco-ulco.fr>
2022-02-22 12:05:58 +0100

    final commit

--- app/src/main/java/com/example/partiel_rezquellah_hiba/MainActivity.java
+++ app/src/main/java/com/example/partiel_rezquellah_hiba/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.partiel_rezquellah_hiba;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.partiel_rezquellah_hiba.interfaceAPI.PiloteService;
@@ -32,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_baseline_account_box_24);
+
+
+
         piloteService.listPilotes().enqueue(new Callback<List<Pilote>>() {
             @Override
             public void onResponse(Call<List<Pilote>> call, Response<List<Pilote>> response) {
@@ -45,6 +55,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void afficherRepos(List<Pilote> pilotes) {
-        Toast.makeText(this,"nombre de dépots : "+pilotes.size(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"nombre de pilotes : "+pilotes.size(), Toast.LENGTH_SHORT).show();
     }
 }
